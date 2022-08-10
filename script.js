25007aceff464c806786db935fc418583a10b676
Gauld0th <shadowrullz@gmail.com>
2022-08-10 16:28:01 -0300

    agregado text area readonly, agregado hover en css file, funcinalidad para desaparecer y aparecer text area

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.querySelector(".buttonEncriptar").onclick = encryptMsg;
 document.querySelector(".buttonDesencriptar").onclick = decryptMsg;
 document.querySelector(".buttonCopiar").onclick = copy;
+document.getElementById("hiddenTextarea2").style.display = "none";
+
 
 function parseMsg(match, replacement) {
     const text = document.querySelector(".insertText").value 
@@ -12,10 +14,15 @@ function parseMsg(match, replacement) {
 }
 
 function decryptMsg() {
+    hideImg();
+    unhideTextarea();
     return parseMsg(/enter|imes|ai|ober|ufat/g, {enter: "e", imes: "i", ai: "a", ober: "o", ufat: "u"});
 }
 
+
 function encryptMsg() {
+    hideImg();
+    unhideTextarea();
     return parseMsg(/[eiaou]/g, {e: "enter", i: "imes", a: "ai", o: "ober", u: "ufat"});
 }
 
@@ -28,4 +35,12 @@ function copy() {
     var copyToClip = document.getElementById("copyToClipboard").value;
     navigator.clipboard.writeText(copyToClip);
     alert("Texto copiado!");
+}
+
+function hideImg(){
+    document.getElementById("hiddenTextarea").style.display = "none";
+}
+
+function unhideTextarea(){
+    document.getElementById("hiddenTextarea2").style.display = "block";
 }
